a279e23edaf9acd099f9e7d6eb4364326f5bacbe
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-12-25 21:09:53 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as express from 'express'
 
 const app = express()
 
-express.static('public')
+app.use(express.static('public'))
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`)
