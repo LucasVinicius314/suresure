b38a2443d20e2f50b7f11c13313237a718905310
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-07-18 05:09:36 -0300

    logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import * as express from 'express'
 
 const app = express()
 
+app.use((req, res, next) => {
+  const ip = req.ip
+
+  console.log('ip: ' + ip)
+
+  for (const key in req.headers) {
+    console.log(`${key}: ${req.headers[key]}`)
+  }
+
+  next()
+})
+
 app.use(express.static('public'))
 
 app.listen(process.env.PORT, () => {
